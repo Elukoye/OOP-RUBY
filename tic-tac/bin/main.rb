5162ba7ecc61d39d1e5e121124e3d9add4130719
IBUNHABIBU <ibunhabib@gmail.com>
2019-08-29 18:54:37 +0300

    adding some files and pseudocode

---
+++ tic-tac/bin/main.rb
@@ -0,0 +1,18 @@
+
+# display welcome message
+# ask for player's name i.e player1 and player2
+# take their inputs and assign tokens i.e "X" , "O"
+# until the game is over 
+# loop 
+# start game
+# ask for player position and validate input
+# if input is valid and position is not taken on the board
+# place token on board
+# check for winning combination
+# if winn 
+# congratulate the winner
+# else if draw inform players
+# break
+# else switch players
+# 
+
