bcd5e1252fefc7eebc207bc4e91b1197cca95367
IBUNHABIBU <ibunhabib@gmail.com>
2019-09-19 19:15:14 +0300

    error correction

--- bin/main.rb
+++ bin/main.rb
@@ -7,15 +7,15 @@ def game_start
   puts "#{@player_1} is  x"
   puts "Player two input name: "
   @player_2 = gets.chomp.capitalize + " [o]"
- puts "#{@player_2} is  o"
+  puts "#{@player_2} is  o"
 end
 
 def take_turns(current_player)     
-    puts "#{current_player} ,choose one among #{@board.rem_nums}: " 
-    position = gets.chomp.to_i
-  
-    while true do
-      if  position.between?(1,9) && !@board.position_taken?(position)
+  puts "#{current_player} ,choose one among #{@board.rem_nums}: " 
+  position = gets.chomp.to_i
+
+  while true do
+    if  position.between?(1,9) && !@board.position_taken?(position)
       position -= 1 
       break
       else        
@@ -28,16 +28,16 @@ def take_turns(current_player)
     @board.replace_var(position,"x")
     else
     @board.replace_var(position,"o")
-    end
+  end
     @board.display
 end 
 
 def switchplayer(c_p)
   returnvar=''
-  if   c_p == @player_1
-      returnvar = @player_2
+  if c_p == @player_1
+    returnvar = @player_2
   else
-      returnvar = @player_1
+    returnvar = @player_1
   end
   return returnvar
 end 
@@ -47,22 +47,22 @@ def game_loop
   game_start 
   c_player = @player_2   
   until false do
-     c_player = switchplayer(c_player)
-     take_turns(c_player)       
-     break if @board.win? || @board.full?             
+    c_player = switchplayer(c_player)
+    take_turns(c_player)       
+    break if @board.win? || @board.full?             
   end 
-   winner = @board.winner
-   mshindi =''     
-   if winner == nil
+  winner = @board.winner
+  mshindi =''     
+  if winner == nil
     puts " Damn ,its a draw!!... PLAY AGAIN?"
-   elsif
+  elsif
     if winner == "x" 
       mshindi=@player_1
     else 
       mshindi=@player_2
     end
     puts "Congrats #{mshindi} , YOU WON!"
-   end
+  end
 end
 
 game=Logic.new
--- lib/board.rb
+++ lib/board.rb
@@ -1,28 +1,28 @@
 class Board
-    def initialize
-      @board =(1..9).to_a 
-      @remval=(1..9).to_a 
-      @winner = nil
-      @piece = ["x","o"]
-    end  
+  def initialize
+    @board =(1..9).to_a 
+    @remval=(1..9).to_a 
+    @winner = nil
+    @piece = ["x","o"]
+  end  
     
-    def replace_var(position,token)   
-      @remval.delete(@board[position])  
-      @board[position] = token 
-    end
+  def replace_var(position,token)   
+    @remval.delete(@board[position])  
+    @board[position] = token 
+  end
   
-    def rem_nums
-      return @remval
-    end
+  def rem_nums
+    return @remval
+  end
     
-    def display
-      (1..9).each do |i|
-        print " | " + @board[i - 1].to_s
-        if i % 3 == 0 
-          print " | \n" 
-        end 
-      end
+  def display
+    (1..9).each do |i|
+    print " | " + @board[i - 1].to_s
+      if i % 3 == 0 
+        print " | \n" 
+      end 
     end
+  end
   
   def position_taken?(position)
     @board[position - 1] == "x" || @board[position - 1] == "o"
@@ -39,23 +39,23 @@ class Board
      [0,4,8],[2,4,6]
     ]
     
-    win_combination.each{ |element|
-     bool_value = [@board[element[0]],@board[element[1]],@board[element[2]]].all? { |i| i == "x" } || [@board[element[0]],@board[element[1]],@board[element[2]]].all? { |i| i == "o" }
+  win_combination.each{ |element|
+    bool_value = [@board[element[0]],@board[element[1]],@board[element[2]]].all? { |i| i == "x" } || [@board[element[0]],@board[element[1]],@board[element[2]]].all? { |i| i == "o" }
      if bool_value
       @winner = @board[element[0]] 
      end
       break  if bool_value
     }    
-      return bool_value
+    return bool_value
   end
   
-   def winner
-     @winner
-   end
+  def winner
+    @winner
+  end
    
-   def full? 
+  def full? 
     @board.all?{|token| token == "x" || token =="o"} 
-   end 
+    end 
   end 
   
   class Logic
@@ -64,4 +64,4 @@ class Board
     @player_2 = "player_2"
     @board = Board.new 
   end
-  end
+end
