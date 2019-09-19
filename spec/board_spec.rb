b648e02ba1e3669f786cbf43ff64d69be3fd9729
IBUNHABIBU <ibunhabib@gmail.com>
2019-09-19 17:58:43 +0300

    testing log_spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -11,6 +11,11 @@ RSpec.describe Board do
         it 'checks if there is space on the board' do
           expect(board.position_taken?(1)).to eql(false)
         end 
+
+        it 'checks if space on board is taken' do
+          board.replace_var(2,"x")
+         expect(board.board).to eql([1,2,"x",4,5,6,7,8,9])
+       end
     end
     
     describe '#win?' do
@@ -44,5 +49,33 @@ RSpec.describe Board do
          expect(board.board).to eql(["x", "o", "x", "o", "x", "o", "x", "o", "x"])
        end
      end 
+
+    describe '#replace_var' do
+      it 'replaces position with token on the board' do
+        board.replace_var(0,"o")
+        expect(board.board).to eql(["o",2,3,4,5,6,7,8,9])
+      end
+    end 
+
+    describe "#winner" do
+      it 'declares the winner' do
+         winner =[board.replace_var(0,"x"),board.replace_var(1,"x"),board.replace_var(2,"x")]
+         board.win?
+        expect(board.winner).to eql(winner[0])
+      end 
+  
+      it 'declares if there is no winner ' do 
+        winner =[board.replace_var(0,"x"),board.replace_var(1,"o"),board.replace_var(2,"x")]
+        board.win?
+        expect(board.winner).to be nil
+      end
+    end
+
+    describe '#rem_nums' do 
+      it 'removes players choice form the array' do
+         board.replace_var(1,"x")
+         expect( board.rem_nums).to eql([1, 3, 4, 5, 6, 7, 8, 9])
+      end
+    end 
 end
 
