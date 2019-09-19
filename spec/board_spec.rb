278863b7c47eddce8457c692cbc0bb0d88ecb298
IBUNHABIBU <ibunhabib@gmail.com>
2019-09-19 14:25:14 +0300

    testing replace_var

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -11,8 +11,19 @@ RSpec.describe Board do
         it 'checks if there is space on the board' do
           expect(board.position_taken?(1)).to eql(false)
         end 
-
     end
     
+    describe '#win?' do
+        it 'checks for no win on board' do
+          expect(board.win?).to be false
+        end
+    
+        it 'checks for win on board' do
+          board.replace_var(0,"x")
+          board.replace_var(1,"x")
+          board.replace_var(2,"x")
+          expect(board.win?).to eql(true)
+        end
+      end
 end
 
